c4729bb240883c2c96e83add43c74550a10cab9c
Sungchul Park <sungchul.dev@gmail.com>
2022-06-16 17:38:07 +0900

    feat(doik-cdk8s): psmdb 오퍼레이터 및 커스텀 리소스 cdk8s 객체로 추상화

---
+++ DOIK-cdk8s/main.test.ts
@@ -0,0 +1,11 @@
+import {MongoDbChart} from './main';
+import {Testing} from 'cdk8s';
+
+describe('Placeholder', () => {
+  test('Empty', () => {
+    const app = Testing.app();
+    const chart = new MongoDbChart(app, 'test-chart');
+    const results = Testing.synth(chart)
+    expect(results).toMatchSnapshot();
+  });
+});
